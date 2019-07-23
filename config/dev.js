21872e15090a24450892b09d9d248d6c2e422a68
Asen Chekov <asenchekov@Users-MacBook-Pro.local>
2019-07-23 18:46:40 +0100

    Initial commit

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+  mongoURI: 'mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb',
+  cookieKey: '123123123'
+};
