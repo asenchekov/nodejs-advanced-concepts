dfca3f8cfb0e7771522c74cc501f9b1de155cde1
Asen Chekov <asenchekov@users-mbp.c.machine-learning-200712.internal>
2019-11-12 20:50:22 +0000

    feature: ci automation

--- config/dev.js
+++ config/dev.js
@@ -3,5 +3,6 @@ module.exports = {
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
   mongoURI: 'mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb',
-  cookieKey: '123123123'
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'
 };
